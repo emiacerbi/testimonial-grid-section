54f9b5c9d96d719048e5d1f65358ab579eeacc06
Emiliano Acerbi <eacerbip@gmail.com>
2022-04-06 22:09:16 -0300

    first-commit

---
+++ styles/theme.js
@@ -0,0 +1,24 @@
+import { extendTheme } from '@chakra-ui/react'
+
+const colors = {
+  neutral: {
+    100: 'hsl(0, 0%, 81%)',
+    200: 'hsl(210, 46%, 95%)',
+  },
+  
+  primary: {
+    100: 'hsl(263, 55%, 52%)',
+    200: 'hsl(217, 19%, 35%)',
+    300: 'hsl(219, 29%, 14%)',
+    400: 'hsl(0, 0%, 100%)'
+  },
+}
+
+const fonts = {
+  heading: "'Barlow Semi Condensed', sans-serif"
+}
+
+const theme = extendTheme({ colors, fonts })
+
+export default theme
+
